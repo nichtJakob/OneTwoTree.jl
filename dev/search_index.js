b8eb2aa04a7f27ac77893cd6a06a22b09392ac4d
Documenter.jl <documenter@juliadocs.github.io>
2024-12-01 12:43:55 +0000

    build based on cb219bf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OneTwoTree","category":"page"},{"location":"#OneTwoTree","page":"Home","title":"OneTwoTree","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OneTwoTree.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OneTwoTree]","category":"page"}]
+}
