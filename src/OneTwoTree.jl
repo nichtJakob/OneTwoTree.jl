9d31163d1fcf9767e6b702a37f18683fdb762c5e
eloinoel <eloi.sandt.games@gmx.de>
2024-12-12 23:26:48 +0100

    feat: load_data from csv function

--- src/OneTwoTree.jl
+++ src/OneTwoTree.jl
@@ -1,8 +1,10 @@
 module OneTwoTree
 
 include("Tree.jl")
+include("utils/load_data.jl")
 
 #Main Module file
 export lessThan, tree_prediction, Node, DecisionTree, print_tree
+export load_data
 
 end # end the module
--- src/utils/load_data.jl
+++ src/utils/load_data.jl
@@ -1,4 +1,5 @@
-
+using DataFrames
+using CSV
 
 function load_data(name)
     datasets = ["fashion_mnist_1000"]
@@ -7,9 +8,13 @@ function load_data(name)
         error("Dataset $name not found. Possible datasets: $datasets")
     end
 
-    data_path = joinpath(dirname(pathof(OneTwoTree)), "..", "test/data/", "$name.csv")
+    data_path = joinpath(@__DIR__, "..", "..", "test", "data", "$name.csv")
 
     if name == "fashion_mnist_1000"
-        df = CSV.read(path)
+        df = DataFrame(CSV.File(data_path))
+        labels = df.label
+        features = Matrix(select(df, Not(:label)))
+        features = transpose(features)
+        return features, labels
     end
 end
