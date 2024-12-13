fefbf9b0d0897bff61dacd2181902ed6421cea1e
Tistou <tistou.fages@ecl21.ec-lyon.fr>
2024-12-13 16:19:48 +0100

    add tree_to_string to export

--- src/OneTwoTree.jl
+++ src/OneTwoTree.jl
@@ -3,7 +3,7 @@ module OneTwoTree
 include("Tree.jl")
 
 #Main Module file
-export lessThan, tree_prediction, Node, DecisionTree, print_tree
+export lessThan, tree_prediction, Node, DecisionTree, tree_to_string, print_tree
 
 end # end the module
 
