3f8b10f2e66298e13c78dfddeeb4cf6f5f1bf7be
Jakob <jakob.balasus@gmail.com>
2024-12-15 19:22:28 +0100

    regression all(approx) - fix

--- test/regression_tests.jl
+++ test/regression_tests.jl
@@ -15,5 +15,5 @@ using OneTwoTree
     #predicting
     r1_predictions = predict(r1_tree, r1_test_features)
 
-    @test isapprox.(r1_predictions, [1.5, 3.5], atol=0.1)
+    @test all(isapprox.(r1_predictions, [1.5, 3.5], atol=0.1))
 end
