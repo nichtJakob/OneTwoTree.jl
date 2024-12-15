238a1b13584f4f8567460de4dc86fc988cfa9172
Jakob <jakob.balasus@gmail.com>
2024-12-15 20:44:57 +0100

    regression depth tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
 using OneTwoTree
 using Test
 
-include("data_tests.jl")
-include("decision_tree_tests.jl")
-include("gini_tests.jl")
+#include("data_tests.jl")
+#include("decision_tree_tests.jl")
+#include("gini_tests.jl")
 #include("cart_tests.jl")
 include("regression_tests.jl")
 
