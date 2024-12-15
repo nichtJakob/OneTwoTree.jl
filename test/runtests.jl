0138916c5cb9190790e156ec16ed2c4c3e46b9b2
Brunololos <70522244+Brunololos@users.noreply.github.com>
2024-12-15 12:13:25 +0100

    Saved debugging tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,25 +13,6 @@ end
 @testset "Tree.jl" begin # Tests the functionality of Node, tree_prediction, less in Tree.jl
 
     @testset "tree_creation" begin
-        dataset = [[3,4] [6,1] [0,2]]
-        cat_labels = ["Chicken", "Egg"]
-        reg_labels = [12.0, 4.5]
-
-        root = Node(dataset, cat_labels, true)
-        root = Node(dataset, reg_labels, false)
-
-        dataset1 = [[3.5,1.0,5.6] [9.1,1.2,3.3] [2.9,0.4,4.3]]
-        dataset2 = [["Snow","Lax","Arm"] ["Hard","Snow","Hard"] ["Arm","Page","Payoff"]]
-        dataset3 = [[3.1,"Lax","Arm"] [0.6,"Snow","Hard"] [4.2,"Page","Payoff"]]
-        cat_labels1 = ["Chicken", "Egg", "Egg"]
-        reg_labels1 = [12.0, 4.5, 6.7]
-
-        root = Node(dataset1, cat_labels1, true)
-        root = Node(dataset1, reg_labels1, false)
-        root = Node(dataset1, cat_labels1, true, max_depth=1)
-        root = Node(dataset1, reg_labels1, false, max_depth=1)
-        root = Node(dataset2, cat_labels1, true, max_depth=1)
-        root = Node(dataset2, reg_labels1, false, max_depth=1)
         # @test tree_prediction(root, [1.0]) == 0.0
         # @test tree_prediction(root, [0.0]) == 1.0
         # @test tree_prediction(root, [55.0]) == 0.0
@@ -50,5 +31,5 @@ end
     include("decision_tree_tests.jl")
 end
 
-include("gini_tests.jl")
+# include("gini_tests.jl")
 
