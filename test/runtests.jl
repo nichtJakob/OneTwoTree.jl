fb8fde6f776270d625a797544b411963ee24e086
eloinoel <eloi.sandt.games@gmx.de>
2024-12-15 15:17:16 +0100

    wip: move jakobs simple tests to other file

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,25 +1,6 @@
 using OneTwoTree
 using Test
 
-function get_test_Tree_less_0_5() # returns 1.0 if the input in dim 1 is less 0.5 else 0.0
-    # TODO: I changed the Node signature, so this needs to be updated as well
-    # leaf1 = Node(prediction=1.0)
-    # leaf2 = Node(prediction=0.0)
-    # root = Node(decision = x -> lessThan(x, 0.5), true_child = leaf1, false_child = leaf2)
-    return root
-end
-
-
-@testset "Tree.jl" begin # Tests the functionality of Node, tree_prediction, less in Tree.jl
-    # @testset "Tree Prediction" begin
-    #     root = get_test_Tree_less_0_5()
-    #     @test tree_prediction(root, [1.0]) == 0.0
-    #     @test tree_prediction(root, [0.0]) == 1.0
-    #     @test tree_prediction(root, [55.0]) == 0.0
-    #     @test tree_prediction(root, [-1.0]) == 1.0
-    # end
-end
-
 include("data_tests.jl")
 include("decision_tree_tests.jl")
 include("gini_tests.jl")
