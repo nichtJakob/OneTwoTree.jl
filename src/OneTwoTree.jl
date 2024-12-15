4037def4d8d873b7ec0edfc3f59a67962e50423a
eloinoel <eloi.sandt.games@gmx.de>
2024-12-15 12:56:00 +0100

    fix: cleanup

--- src/OneTwoTree.jl
+++ src/OneTwoTree.jl
@@ -1,11 +1,10 @@
 module OneTwoTree
 
+#Main Module file
 include("Tree.jl")
 include("utils/load_data.jl")
 include("CARTutils.jl")
 include("Gini.jl")
-
-#Main Module file
 
 export Node, DecisionTree
 export predict, fit!, build_tree, print_tree
@@ -14,9 +13,5 @@ export lessThanOrEqual, equal
 export load_data
 export gini_impurity
 
-
-
 end # end the module
 
-
-#Test
