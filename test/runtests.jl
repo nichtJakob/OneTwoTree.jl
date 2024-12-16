f0e36bb81812249262aef59a1b63cb8c87dc221b
Brunololos <70522244+Brunololos@users.noreply.github.com>
2024-12-16 16:01:23 +0100

    Setup cart utils test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Test
 include("data_tests.jl")
 include("decision_tree_tests.jl")
 include("gini_tests.jl")
-#include("cart_tests.jl")
+include("cart_tests.jl")
 include("regression_tests.jl")
+include("cart_utils_tests.jl")
 
