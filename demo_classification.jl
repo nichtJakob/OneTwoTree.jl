262587859947eb94719b33461e4fd4fad3f17d9b
eloinoel <eloi.sandt.games@gmx.de>
2025-01-24 15:12:36 +0100

    fixed demo_classification to work with new package dependencies

--- demo_classification.jl
+++ demo_classification.jl
@@ -3,15 +3,17 @@
 using OneTwoTree
 
 # if you want to execute this example in your julia REPL you will first need to
-# 1) install a dependency for the dataset:
+# 1) install dependencies for the dataset:
 #   julia>  using Pkg
 #   julia>  Pkg.add("MLDatasets")
+#   julia>  Pkg.add("DataFrames")
 #
 # 2) execute the code in your REPL:
 #   julia>  include("demo_classification.jl")
 
 # first we load the iris dataset. Targets are the 3 types of the flowers
 # and data contains measurements of flowers
+using DataFrames
 using MLDatasets: Iris
 
 dataset = Iris()
@@ -29,9 +31,10 @@ println("Size of data: ", size(data), "\n")
 # here we define how big training and test set should be
 # you can modify the splitting point to be any value between 1 and 149
 splitting_point = 120
-println("It is $(splitting_point > 1 && splitting_point < 150) that you have chosen a qualifying splitting point $(splitting_point)", "\n")
 
-
+if splitting_point < 1 || splitting_point > 150
+    error("You have chosen an invalid splitting point. Please choose a value between 1 and 150")
+end
 
 # We split the data in training and test sets
 train_data = data[1:splitting_point, :]
@@ -58,19 +61,20 @@ test_predictions = predict(tree, test_data)
 accuracy = sum(test_predictions .== test_labels) / length(test_labels)
 
 println("\n\nFor the Iris dataset we have achieved a test accuracy of $(round(accuracy * 100, digits=2))%")
-
+println("-----------------------------------------------------\n")
 
 # now let's try using random forests
-println("\n\n Now we will grow our random forest")
 
 # you can play around with the following values of the parameters:
 # n_trees := number of trees in our forest
 # n_features_per_tree := number of features per tree
 # max_depth := our max depth for the trees
 
-forest = ForestClassifier(n_trees=5, n_features_per_tree=30, max_depth=5)
+n_features_per_tree = 30
+println("\n\nNow we will grow our random forest containing 5 trees with $n_features_per_tree features per tree and a max depth of $our_max_depth")
+
+forest = ForestClassifier(n_trees=5, n_features_per_tree=n_features_per_tree, max_depth=our_max_depth)
 fit!(forest, train_data, train_labels)
-println("The forest has been grown")
 
 # let's have a look at our forest
 print_forest(forest)
