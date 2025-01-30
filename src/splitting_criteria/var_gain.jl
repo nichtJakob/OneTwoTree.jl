b75fb0e7da776ac23f747418d26a88ce1893e4c3
Brunololos <70522244+Brunololos@users.noreply.github.com>
2025-01-30 19:11:57 +0100

    120 delete old splitting criterion signatures (#124)
    * removed old gini_impurity signature & updated tests
    
    * removed old information gain signature
    
    * Removed old variance gain signatures
    
    * Updated variance & information gain docsstrings

--- src/splitting_criteria/var_gain.jl
+++ src/splitting_criteria/var_gain.jl
@@ -1,7 +1,6 @@
 #using StatsBase
 
 function variance(data::AbstractVector) :: Float64
-    #TODO different datatypes?
     if isempty(data)
         return 0.0
     end
@@ -10,46 +9,19 @@ function variance(data::AbstractVector) :: Float64
     return variance
 end
 
-function variance(labels::AbstractVector, node_data::Vector{Int64}) :: Float64
-    if isempty(labels)
-        return 0.0
-    end
-    # TODO: here we copy a lot, but I wasnt too confident to change it on release day
-    mean_value = mean(labels[node_data])
-    variance = mean((labels[node_data] .- mean_value) .^ 2)
-    return variance
-end
+"""
+    variance_gain(parent_labels::AbstractVector, true_child_labels::AbstractVector, false_child_labels) -> Float64
 
-# function variance(child_1_labels::AbstractVector, child_2_labels::AbstractVector) :: Float64
-function variance(features::AbstractMatrix, labels::AbstractVector, node_data::Vector{Int64}, decision_fn::Function, decision_param::Union{Real, String}, decision_feature::Int64)::Float64
+This function calculates the variance gain for a split in a decision tree. The split is characterized by the partition of the parent_labels into true_child_labels and false_child_labels according to some discriminant function.
 
-    # Split data in true and false
-    split_true, split_false = split_indices(features, node_data, decision_fn, decision_param, decision_feature)
+# Arguments:
+- `parent_labels`: A vector of data labels (e.g., classes or numerical values in the case of regression).
+- `true_child_labels`: A vector of a subset of data labels contained in parent_labels.
+- `false_child_labels`: A vector of a subset of data labels contained in parent_labels.
 
-    # Labeling data
-    true_labels = labels[split_true]
-    false_labels = labels[split_false]
-    total_labels = length(labels[node_data])
-
-    if isempty(true_labels) || isempty(false_labels)
-        return 0.0
-    end
-
-    true_weight = length(true_labels) / total_labels
-    false_weight = length(false_labels) / total_labels
-
-    weighted_var_1 = true_weight * variance(true_labels)
-    weighted_var_2 = false_weight * variance(false_labels)
-    weighted_var = weighted_var_1 + weighted_var_2
-
-    return weighted_var
-end
-
-function variance_gain(features::AbstractMatrix, labels::AbstractVector, node_data::Vector{Int64}, decision_fn::Function, decision_param::Union{Real, String}, decision_feature::Int64)::Float64
-    return variance(labels, node_data) - variance(features, labels, node_data, decision_fn, decision_param, decision_feature)
-end
-
-# # Splitting criterion for regression based on the variance similar to infogain
+# Returns:
+- The variance gain of the split.
+"""
 function variance_gain(parent_labels::AbstractVector, true_child_labels::AbstractVector, false_child_labels::AbstractVector) :: Float64
     max_gain = variance(parent_labels)
 
