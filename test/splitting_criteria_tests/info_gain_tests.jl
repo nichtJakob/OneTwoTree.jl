894a5662b335df32ecec664c4f4d5a5eee0bd44a
nichtJakob <109101949+nichtJakob@users.noreply.github.com>
2025-01-30 19:07:38 +0100

    more infogain tests (#115)

--- test/splitting_criteria_tests/info_gain_tests.jl
+++ test/splitting_criteria_tests/info_gain_tests.jl
@@ -35,4 +35,43 @@ using Test
         @test isa(info_gain_result, Float64)
         @test isapprox(info_gain_result, 0, atol=0)
     end
+
+    @testset "Spliting relation" begin
+        parent_labels = [1, 0, 1, 0, 2, 2]
+
+        #worse split
+        child_1_labels = [1, 1, 2]
+        child_2_labels = [0, 0, 2]
+        info_gain_result_worse = information_gain(parent_labels, child_1_labels, child_2_labels)
+        @test isa(info_gain_result_worse, Float64)
+
+        #better split
+        better_1_labels = [1, 1]
+        better_2_labels = [0, 0, 2, 2]
+        info_gain_result_better = information_gain(parent_labels, better_1_labels, better_2_labels)
+        @test isa(info_gain_result_better, Float64)
+
+        @test info_gain_result_better > info_gain_result_worse
+    end
+
+    @testset "Char labels with swapped childs" begin
+        a = 'a'
+        b = 'b'
+        c = 'c'
+        parent_labels = [a, b, b, c, c, c]
+
+        #split
+        child_1_labels = [a, b, b]
+        child_2_labels = [c, c, c]
+        info_gain_result = information_gain(parent_labels, child_1_labels, child_2_labels)
+        @test isa(info_gain_result, Float64)
+        @test isapprox(info_gain_result, 0.5, atol=0.5)
+
+        #same split but swaoped childs 
+        info_gain_result_swapped = information_gain(parent_labels, child_2_labels, child_1_labels)
+        @test isa(info_gain_result_swapped, Float64)
+
+        @test info_gain_result == info_gain_result_swapped
+    end
+
 end
